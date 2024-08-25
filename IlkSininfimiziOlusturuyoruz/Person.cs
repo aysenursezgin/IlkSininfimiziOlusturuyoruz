2a8a3956b19e0676c2ae144dfd15d8fa92abe262
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-08-25 16:59:34 +0300

    Proje dosyası ekle.

---
+++ IlkSininfimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkSininfimiziOlusturuyoruz
+{
+    //istenilen ozellikleri tanimliyoruz
+    public class Person
+    {
+
+      
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string DogumTarihi { get; set; }
+    }
+}
---
+++ IlkSininfimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using IlkSininfimiziOlusturuyoruz;
+
+
+// egitmen ve ogrencileri tanimliyoruz
+
+Person egitmen1 = new Person()
+{
+    Ad = "Hatice",
+    Soyad = "Ekşi",
+    DogumTarihi = "06,06,1996"
+};
+
+Person egitmen2 = new Person()
+{
+    Ad = "Kübra",
+    Soyad = "Ocaktar",
+    DogumTarihi = "24,04,1995"
+};
+
+Person ogrenci1 = new Person()
+{
+    Ad = "Ayşenur",
+    Soyad = "Sezgin",
+    DogumTarihi = "01,04,1994"
+};
+
+Person ogrenci2 = new Person()
+{
+    Ad = "Fatih",
+    Soyad = "Sezgin",
+    DogumTarihi = "03,04,1996"
+};
+
+// Konsol ekranına yazdırıyoruz
+
+Console.WriteLine(egitmen1.Ad);
+Console.WriteLine(egitmen1.Soyad);
+Console.WriteLine(egitmen1.DogumTarihi);
+Console.WriteLine("---------------------------------------------");
+
+Console.WriteLine(egitmen2.Ad);
+Console.WriteLine(egitmen2.Soyad);
+Console.WriteLine(egitmen2.DogumTarihi);
+Console.WriteLine("---------------------------------------------");
+
+Console.WriteLine(ogrenci1.Ad);
+Console.WriteLine(ogrenci1.Soyad);
+Console.WriteLine(ogrenci1.DogumTarihi);
+Console.WriteLine("---------------------------------------------");
+
+Console.WriteLine(ogrenci2.Ad);
+Console.WriteLine(ogrenci2.Soyad);
+Console.WriteLine(ogrenci2.DogumTarihi);
+Console.WriteLine("---------------------------------------------");
